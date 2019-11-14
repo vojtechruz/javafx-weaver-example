818d73aabfaf77b7e3db9723c1c3de2a051348e0
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2019-11-14 14:07:42 +0100

    Simplified the design

--- src/main/java/com/vojtechruzicka/javafxweaverexample/JavaFxApplication.java
+++ src/main/java/com/vojtechruzicka/javafxweaverexample/JavaFxApplication.java
@@ -2,7 +2,10 @@ package com.vojtechruzicka.javafxweaverexample;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import net.rgielen.fxweaver.core.FxWeaver;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
@@ -21,7 +24,11 @@ public class JavaFxApplication extends Application {
 
     @Override
     public void start(Stage stage) {
-        applicationContext.publishEvent(new StageReadyEvent(stage));
+        FxWeaver fxWeaver = applicationContext.getBean(FxWeaver.class);
+        Parent root = fxWeaver.loadView(MyController.class);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @Override
--- src/main/java/com/vojtechruzicka/javafxweaverexample/MyController.java
+++ src/main/java/com/vojtechruzicka/javafxweaverexample/MyController.java
@@ -1,12 +1,9 @@
 package com.vojtechruzicka.javafxweaverexample;
 
 import net.rgielen.fxweaver.core.FxmlView;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @FxmlView("main-stage.fxml")
 public class MyController {
-
 }
